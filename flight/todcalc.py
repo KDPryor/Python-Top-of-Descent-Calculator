d0c42b204630ed3e487eb987400a3f0d87dcb166
Ken <kdpryor@gmail.com>
2019-03-17 22:13:32 -0500

    Update todcalc.py
    Added info, corrected spelling.

--- flight/todcalc.py
+++ flight/todcalc.py
@@ -1,13 +1,13 @@
 #! Python3
-"""Simple script to calculate your top of descent."""
+"""Simple script to calculate top of descent. For flight simulation only. Not to be used for real life navigation."""
 import click
 import logging
 
 
-def calculate_decent_rate(target, start,  decent_rate):
-    """Calculate decent rate from required altitude."""
+def calculate_descent_rate(target, start,  descent_rate):
+    """Calculate descent rate from required altitude."""
     difference = start - target
-    time = int(difference / decent_rate)
+    time = int(difference / descent_rate)
     distance = (difference * 3) / 1000
     return_data = {"time": time, "distance": distance}
     return return_data
@@ -16,14 +16,14 @@ def calculate_decent_rate(target, start,  decent_rate):
 @click.command()
 @click.option('--target', '-t', prompt='Target Altitude:', help='Target Altitude.', required=True, type=int)
 @click.option('--start', '-s',  prompt='Starting Altitude: ', help='Starting Altitude.', required=True, type=int)
-@click.option('--decent_rate', '-dr', prompt='Decent Rate: ', help='Descent Rate.', required=True, type=int)
-def cli(target, start,  decent_rate):
+@click.option('--descent_rate', '-dr', prompt='Descent Rate: ', help='Descent Rate.', required=True, type=int)
+def cli(target, start,  descent_rate):
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
     logger.addHandler(stream_handler)
-    calculation_result = calculate_decent_rate(target, start, decent_rate)
+    calculation_result = calculate_descent_rate(target, start, descent_rate)
     # Print the answers!
     logging.info(" Start descent: " + str(calculation_result["time"]) + " minutes prior to target location")
     logging.info(" Start descent: " + str(calculation_result["distance"]) + " miles from target location")
