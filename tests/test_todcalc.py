2099556e625ee6b81e14483ca0b1facb674432c5
Wyatt Roersma <wyattroersma@gmail.com>
2019-04-10 14:13:58 -0600

    fix unit tests with proper spelling of function and variable

--- tests/test_todcalc.py
+++ tests/test_todcalc.py
@@ -2,10 +2,9 @@
 """Default unittests to automate functional testing of flight module code."""
 # !/usr/bin/env python
 import unittest
-import click
 from click.testing import CliRunner
 
-from flight.todcalc import cli, calculate_decent_rate
+from flight.todcalc import cli, calculate_descent_rate
 
 
 class UserModelCase(unittest.TestCase):
@@ -13,7 +12,7 @@ class UserModelCase(unittest.TestCase):
 
     def test_decent_rate(self):
         """Authentication unit test function."""
-        return_data = calculate_decent_rate(1500, 5500, 300)
+        return_data = calculate_descent_rate(1500, 5500, 300)
         self.assertEqual(return_data['time'], 13)
         self.assertEqual(return_data['distance'], 12.0)
 
@@ -21,7 +20,7 @@ class UserModelCase(unittest.TestCase):
         """Authentication unit test function."""
         runner = CliRunner()
         result = runner.invoke(
-            cli, ["--target", "1500", "--start", "5500", "--decent_rate", "300"])
+            cli, ["--target", "1500", "--start", "5500", "--descent_rate", "300"])
         assert not result.exception
 
 
